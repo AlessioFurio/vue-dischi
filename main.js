5aed741677764e12ba08b7d639405608a6274fb7
Furio Alessio <alefurio@hotmail.it>
2020-11-23 18:51:43 +0100

    stampa on select option

--- main.js
+++ main.js
@@ -5,13 +5,33 @@ var app = new Vue({
 
 	data: {
 		dischi: [],
+		generi: ['all'],
+		key: 'all',
+		bool: false,
 	},
+
+	methods: {
+		onChange(event) {
+            console.log(event.target.value);
+			this.bool = true;
+        }
+
+	}, // fine methods
 
 	mounted() {
 			axios
 			.get('https://flynn.boolean.careers/exercises/api/array/music')
 			.then((risposta) =>{
-				this.dischi = risposta.data.response;
+
+				this.dischi = risposta.data.response; // assegno ad dischi l'array response
+				this.dischi.forEach((element, index) =>{
+					// this.generi.push(element.genre);
+
+					if(!this.generi.includes(element.genre)){ // se il genere non e' presente
+						this.generi.push(element.genre); // pusho in generi
+					}
+				});
+				console.log(this.generi);
 
 
 			}); // fine then
